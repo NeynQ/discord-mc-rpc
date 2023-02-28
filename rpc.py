99f6e14087ac889b2ef2057c0ffcb70702a17191
NeynQ <neynqq@gmail.com>
2023-02-28 15:48:13 +0300

    ok

---
+++ rpc.py
@@ -0,0 +1,20 @@
+from pypresence import Presence
+from yandex_music import Client
+import time
+
+server = input("Введите адрес сервера: ")
+version = input("Введите версию Minecraft: ")
+
+client_id = "1080098980765110362"
+RPC = Presence(client_id)
+RPC.connect()
+
+while True:
+    RPC.update(
+        state = "Версия: " + version,
+        details = "Сервер: " + server,
+        start = time.time(),
+        large_image = "mc_avatar",
+        large_text = "Minecraft"
+    )
+    time.sleep(15)
